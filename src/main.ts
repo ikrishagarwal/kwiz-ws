b2f1bb2e925c6ddc659902a5cd9e7eed9736cdea
Krish <68506145+ikrishagarwal@users.noreply.github.com>
2024-07-17 02:17:06 +0530

    refactor: code cleanup

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { readFileSync } from "fs";
 import { createServer } from "http";
 import WebSocket, { WebSocketServer } from "ws";
 import { formatJson } from "#root/helpers";
+import { checkAttendee, checkOrganizer, roomExists } from "./utils.js";
 
 const PORT = process.env.PORT || 8080;
 const DEV = process.argv.at(2) === "dev";
@@ -9,28 +10,32 @@ const DEV = process.argv.at(2) === "dev";
 console.log("Dev mode:", DEV);
 
 const server = createServer(async (req, res) => {
-  // For dev purposes
-  const html = readFileSync("./static/index.html", "utf8");
-  const js = readFileSync("./static/script.js", "utf8");
+  try {
+    const html = readFileSync("./static/index.html", "utf8");
+    const js = readFileSync("./static/script.js", "utf8");
 
-  if (DEV) {
-    switch (req.url) {
-      case "/script.js":
-        res.writeHead(200, { "Content-Type": "application/javascript" });
-        res.end(js);
-        break;
+    if (DEV) {
+      switch (req.url) {
+        case "/script.js":
+          res.writeHead(200, { "Content-Type": "application/javascript" });
+          res.end(js);
+          break;
 
-      case "/":
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(html);
-        break;
+        case "/":
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(html);
+          break;
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Websocket server running");
     }
-  } else {
+  } catch {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Websocket server running");
   }
 });
-// Shared state
+
 const state: StatsType = {
   traffic: 0,
   rooms: {},
@@ -55,141 +60,144 @@ wss.on("connection", (ws: WSExtended) => {
     }
 
     switch (request.request_type) {
-      case "host_room":
-        if (request.designation === "organizer") {
-          if (!request.roomId)
-            return ws.send(
-              formatJson({ error: true, message: "Invalid room id" })
-            );
-
-          ws.send(formatJson({ success: true, message: "Room hosted" }));
-        } else {
-          ws.send(
+      case RequestType.HOST_ROOM:
+        if (!checkOrganizer(request.designation))
+          return ws.send(
             formatJson({
               error: true,
               message: "Designation does not support hosting",
             })
           );
-        }
+
+        if (!request.roomId)
+          return ws.send(
+            formatJson({ error: true, message: "Invalid room id" })
+          );
+
+        state.rooms[request.roomId] = {
+          question: "",
+          options: [],
+        };
+
+        ws.send(formatJson({ success: true, message: "Room hosted" }));
         break;
 
-      case "register_user":
-        if (request.designation === "attendee") {
-          if (!state.rooms[request.roomId])
-            return ws.send(
-              formatJson({ error: true, message: "Room does not exist" })
-            );
-
-          if (!request.username || !request.userId)
-            return ws.send(
-              formatJson({ error: true, message: "Invalid user id" })
-            );
-
-          //   state.rooms[request.roomId].attendees.push({
-          //     name: request.username,
-          //     id: request.userId,
-          //   });
-
-          ws.data = {
-            roomId: request.roomId,
-          };
-          ws.send(formatJson({ success: true, message: "User registered" }));
-        } else {
-          ws.send(
+      case RequestType.REGISTER_USER:
+        if (!checkAttendee(request.designation))
+          return ws.send(
             formatJson({
               error: true,
               message: "Designation does not support registration",
             })
           );
-        }
+
+        if (!roomExists(Object.keys(state.rooms), request.roomId))
+          return ws.send(
+            formatJson({ error: true, message: "Room does not exist" })
+          );
+
+        if (!request.username || !request.userId)
+          return ws.send(
+            formatJson({ error: true, message: "Invalid user id" })
+          );
+
+        //   state.rooms[request.roomId].attendees.push({
+        //     name: request.username,
+        //     id: request.userId,
+        //   });
+
+        ws.data = {
+          roomId: request.roomId,
+        };
+        ws.send(formatJson({ success: true, message: "User registered" }));
         break;
 
-      case "add_question":
-        if (request.designation === "organizer") {
-          if (!state.rooms[request.roomId])
-            return ws.send(
-              formatJson({ error: true, message: "Room does not exist" })
-            );
-
-          if (!request.question)
-            return ws.send(
-              formatJson({ error: true, message: "Invalid question" })
-            );
-
-          if (!request.options || request.options.length !== 4)
-            return ws.send(
-              formatJson({ error: true, message: "Invalid options" })
-            );
-
-          state.rooms[request.roomId].question = request.question;
-          state.rooms[request.roomId].options = request.options;
-
-          wss.clients.forEach((client) => {
-            if ((client as WSExtended).data.roomId === request.roomId) {
-              client.send(
-                formatJson({
-                  action: "add_question",
-                  message: "Question added",
-                  question: request.question,
-                  options: request.options,
-                })
-              );
-            }
-          });
-          ws.send(formatJson({ success: true, message: "Question added" }));
-
-          // TODO: send the question to attendees
-        } else {
-          ws.send(
+      case RequestType.ADD_QUESTION:
+        if (!checkOrganizer(request.designation))
+          return ws.send(
             formatJson({
               error: true,
               message: "Designation does not support adding questions",
             })
           );
-        }
+
+        if (!roomExists(Object.keys(state.rooms), request.roomId))
+          return ws.send(
+            formatJson({ error: true, message: "Room does not exist" })
+          );
+
+        if (!request.question.trim())
+          return ws.send(
+            formatJson({ error: true, message: "Empty question" })
+          );
+
+        if (
+          !request.options ||
+          request.options.length !== 4 ||
+          !request.options.every((option: string) => option.trim())
+        )
+          return ws.send(
+            formatJson({ error: true, message: "Invalid options" })
+          );
+
+        state.rooms[request.roomId].question = request.question;
+        state.rooms[request.roomId].options = request.options;
+
+        wss.clients.forEach((client) => {
+          if ((client as WSExtended).data.roomId === request.roomId) {
+            client.send(
+              formatJson({
+                action: "add_question",
+                message: "Question added",
+                question: request.question,
+                options: request.options,
+              })
+            );
+          }
+        });
+        ws.send(formatJson({ success: true, message: "Question added" }));
         break;
 
-      case "submit_answer":
-        if (request.designation === "organizer") {
-          if (!state.rooms[request.roomId])
-            return ws.send(
-              formatJson({ error: true, message: "Room does not exist" })
-            );
-
-          if (isNaN(request.answer))
-            return ws.send(
-              formatJson({ error: true, message: "Invalid answer" })
-            );
-
-          if (!state.rooms[request.roomId].question)
-            return ws.send(
-              formatJson({ error: true, message: "No question added" })
-            );
-
-          wss.clients.forEach((client) => {
-            if ((client as WSExtended).data.roomId === request.roomId) {
-              client.send(
-                formatJson({
-                  action: "submit_answer",
-                  message: "Answer submitted",
-                  answer: request.answer,
-                })
-              );
-            }
-          });
-          ws.send(formatJson({ success: true, message: "Answer submitted" }));
-        } else {
-          ws.send(
+      case RequestType.SUBMIT_ANSWER:
+        if (!checkOrganizer(request.designation))
+          return ws.send(
             formatJson({
               error: true,
               message: "Designation does not support submitting answers",
             })
           );
-        }
+
+        if (!roomExists(Object.keys(state.rooms), request.roomId))
+          return ws.send(
+            formatJson({ error: true, message: "Room does not exist" })
+          );
+
+        if (isNaN(request.answer))
+          return ws.send(
+            formatJson({ error: true, message: "Invalid answer index" })
+          );
+
+        if (!state.rooms[request.roomId].question)
+          return ws.send(
+            formatJson({ error: true, message: "No question added" })
+          );
+
+        wss.clients.forEach((client) => {
+          if ((client as WSExtended).data.roomId === request.roomId) {
+            client.send(
+              formatJson({
+                action: "submit_answer",
+                message: "Answer submitted",
+                answer: request.answer,
+              })
+            );
+          }
+        });
+        ws.send(formatJson({ success: true, message: "Answer submitted" }));
         break;
 
       default:
-        ws.send(formatJson({ error: true, message: "Invalid request_type" }));
+        ws.send(formatJson({ error: true, message: "Invalid request type" }));
     }
   });
 
@@ -211,3 +219,10 @@ type StatsType = {
 };
 
 type WSExtended = WebSocket & { data: { roomId: string } };
+
+enum RequestType {
+  HOST_ROOM = "host_room",
+  REGISTER_USER = "register_user",
+  ADD_QUESTION = "add_question",
+  SUBMIT_ANSWER = "submit_answer",
+}
